6974518a656fd4354764636fa8fcd34340282578
emirakbas <makbas1@ogr.iu.edu.tr>
2024-09-03 16:45:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('#addButton').addEventListener('click', function() {
+    console.log('Button clicked');
+    const listElement = document.querySelector('#inputField').value;
+    console.log(`Girilen değer: "${listElement}"`);
+    if (listElement === '') {
+        alert('Lütfen değer giriniz!');
+        return;
+    }
+
+const listCreate = document.createElement('li');
+console.log(listCreate);
+listCreate.textContent = listElement;
+
+document.querySelector('.elementList').appendChild(listCreate);
+
+inputField.value = ''; // input alanını temizler.
+
+});
